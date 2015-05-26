1c4c86d8db433368a238f93ad6c0b112d45ef150
surbut <sarahmurbut@gmail.com>
2015-05-26 10:51:44 -0500

    added html plot functions

--- R/main.R
+++ R/main.R
@@ -581,4 +581,78 @@ plotting.func=function(j,posterior.means,lfsr.mat,marginal.var,genesnpnames,tiss
 
 
 
+checkfunc.html=function(j,b.gp.hat,se.gp.hat,A,k ) {
+  post.means=readRDS(file=paste0("post.means",A,".rds"))
+  post.covs=readRDS(file=paste0("post.covs",A,".rds"))
+  post.ups=readRDS(file=paste0("post.ups",A,".rds"))
+  post.nulls=readRDS(file=paste0("post.nulls",A,".rds"))
+  post.downs=readRDS(file=paste0("post.downs",A,".rds"))
+  posterior.means=read.table(file=paste0("post.mean.",A,".txt"))
+  post.weights=readRDS(paste0("post.weight.",A,".rds"))
+  
+  b.mle=t(as.vector(b.gp.hat[j,]))
+  V.gp.hat=diag(se.gp.hat[j,])^2
+  V.gp.hat.inv <- solve(V.gp.hat)
+  
+  U.gp1kl <- post.b.gpkl.cov(V.gp.hat.inv, covmat[[k]])
+  
+  par(mfrow=c(1,3))
+  plot(diag(U.gp1kl),post.covs[j,k,1:R])
+  
+  
+  
+  mu.gp1kl <- as.array(post.b.gpkl.mean(b.mle, V.gp.hat.inv, U.gp1kl))
+  plot(mu.gp1kl,post.means[j,k,])
+
+
+  plot((post.weights[j,]%*%post.means[j,,]),posterior.means[j,])
+
+  
+}
+
+
+
+plotting.func.html=function(j,posterior.means,lfsr.mat,marginal.var,genesnpnames,tissue.names,mle.mat){
+  
+  R=ncol(posterior.means)
+  posterior.means=as.matrix(posterior.means)
+  col.mat=NULL
+  
+  for(r in 1:R){
+    
+    if (lfsr.mat[j,r]<=0.10) {
+      col.mat[r]=1
+    } else if (lfsr.mat[j,r]<0.5) {
+      col.mat[r]=2
+    } else if (lfsr.mat[j,r]>=0.50) {
+      col.mat[r]=3
+    } 
+  }
+  
+  par(mfrow=c(1,3))
+  
+  b=barplot((posterior.means[j,]),main=paste0("PostTissueMeans,",genesnpnames[j]),names=tissue.names,col=col.mat,las=2,ylim=c((min(posterior.means[j,])-0.05),(max(posterior.means[j,])+0.10)),cex.names=0.5,ylab="PosteriorMean")
+  lfsr=lfsr.mat[j,]
+  mean=as.numeric(posterior.means[j,])
+  sd=as.numeric(sqrt(marginal.var[j,]))
+  segments(b, mean - sd, b, mean + sd, lwd=2)
+
+  
+  mle=mle.mat[j,]
+  a.m=(mle)
+  a.p=(posterior.means[j,])
+  low=min(a.m-0.01,a.p-0.01)
+  high=max(a.m+0.01,a.p+0.01)
+  
+  
+  plot(as.matrix(mle),as.matrix(posterior.means[j,]),main=paste0("MLEvsPostMeans,",genesnpnames[j]),ylim=c(low,high),xlim=c(low,high))
+  abline(0,1,col="red")
+
+  
+  
+  plot(seq(1:R),lapply(1:44,function(x){mean(abs(mle.mat[,x])>=posterior.means[,x])}),ylim=c(0,1),main="|MLE|>|Posteriormean|")
+  dev.off()
+}
+
+
 
