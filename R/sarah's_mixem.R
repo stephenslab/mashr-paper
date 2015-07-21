a70905ab18f16cfe9ab5d2506a7b49a6dca308fb
Sarah Margaret Urbut <sarahmurbut@gmail.com>
2015-07-21 06:36:56 -0500

    modifications

--- R/sarah's_mixem.R
+++ R/sarah's_mixem.R
@@ -257,7 +257,14 @@ deconvolution.em <- function(t.stat,factor.mat,lambda.mat,K,P,permsnp=1000){
 }
 
 
-
+#' @title get.prior.covar.with.max.step
+#' @details wrapper to compute denoised estimates of the fuller rank covariance matrices
+#' @param t.stat
+#' @param P = rank of PC approxiatmion
+#' @param Q = rank of SFA approximation
+#' @param permsnp = number of strong stats you want to train on (default is 1000)
+#' @return a 2 element list of K pis and the KxRxR true.covariance arrays
+#' @export
 
 get.prior.covar.with.max.step <- function(P, X.c,max.step,lambda.mat, Q, factor.mat,omega.table,bma=TRUE)  {
   test=list()
@@ -384,5 +391,168 @@ test.funct=function(j,max.step.unlist,k,R){
 }
 
 
+
+
+#' @title get.prior.covar.with.all.max.step
+#' @details wrapper to compute denoised estimates of the fuller rank covariance matrices
+#' @param t.stat
+#' @param P = rank of PC approxiatmion
+#' @param Q = rank of SFA approximation
+#' @param permsnp = number of strong stats you want to train on (default is 1000)
+#' @return a 2 element list of K pis and the KxRxR true.covariance arrays
+#' @export
+
+get.prior.covar.with.all.max.step <- function(X.c,max.step,lambda.mat, Q, factor.mat,omega.table,bma=TRUE)  {
+  test=list()
+  R=ncol(X.c)
+  M=nrow(X.c)
+  for(l in 1:nrow(omega.table)){
+    test[[l]]=list()
+    omega=omega.table[l,]
+    test[[l]][[1]]=omega*diag(1,R)
+    data.prox=max.step$true.covs[1,,]
+    d.norm=data.prox/max(diag(data.prox))
+    
+    
+    test[[l]][[2]]=omega*d.norm
+    
   
+    
+    cov.pc=max.step$true.covs[3,,]
+    
+    
+    cov.pc.norm=cov.pc/max(diag(cov.pc))
+    
+    
+    
+    test[[l]][[3]]=omega*(cov.pc.norm)
+    if(Q!=0){for(q in 1:Q){
+      
+      load=as.matrix(lambda.mat[,q])
+      fact=as.matrix(factor.mat[q,])
+      rank.prox=load%*%t(fact)
+      a=(1/M*(t(rank.prox)%*% rank.prox))
+      a[is.nan(a)] = 0
+      a.norm=a/max(diag(a))
+      test[[l]][[q+3]]=omega*a.norm
+    }}
+    full.rank=max.step$true.covs[2,,]
+    b.norm=full.rank/max(diag(full.rank))
+    test[[l]][[Q+4]]=omega*b.norm
+    
+    if(bma==TRUE){
+      configs=matrix(0,nrow=R,ncol=R)
+      
+      R=ncol(factor.mat)
+      for(r in 1:R){
+        configs[r,r]=1}
+      
+      configs=rbind(configs,rep(1,R))
+      for(c in 1:nrow(configs)) {
+        
+        mat=(configs[c,]%*%t(configs[c,]))
+        
+        test[[l]][[Q+4+c]]=omega*mat}}}
+  return(U.0kl=test)
+}
+
   
+#' @title compute.hm.covmat.all.max.step
+#' @details use a prespecified X`X from the output of the 'deconvoluting EM'
+#' @param max.step: the list ouput of deconvolution EM (list of length 2, contains the denoised covariance  matrices and vector of pi)
+#' @param Q number of single rank factors to include in the set of covariance matrices
+#' @param P PC approximation
+#' @param BMA Whether or not to include singleton and full configurations
+#' @return a list of covariance matrices
+
+compute.hm.covmat.all.max.step = function(t.stat,v.j,Q,lambda.mat,A,factor.mat,max.step){
+  X.real=as.matrix(t.stat)
+  X.c=apply(X.real,2,function(x) x-mean(x)) ##Column centered matrix of t statistics
+  R=ncol(X.c)
+  omega=mult.tissue.grid(mult=sqrt(2),t.stat,v.j)
+  omega.table=data.frame(omega)
+  lambda.mat=lambda.mat
+  A=A
+  factor.mat=factor.mat
+  U.0kl=get.prior.covar.with.all.max.step(X.c,max.step = max.step,lambda.mat = lambda.mat,Q = Q,factor.mat = factor.mat,omega.table=omega.table,bma = TRUE)
+  covmat=unlist(U.0kl,recursive=F)
+  saveRDS(covmat,paste0("covmat",A,".rds"))
+  
+  return(covmat)}
+  
+
+#' @title get.prior.covar.with.all.max.step
+#' @details wrapper to compute denoised estimates of the fuller rank covariance matrices
+#' @param t.stat
+#' @param P = rank of PC approxiatmion
+#' @param Q = rank of SFA approximation
+#' @param permsnp = number of strong stats you want to train on (default is 1000)
+#' @return a 2 element list of K pis and the KxRxR true.covariance arrays
+#' @export
+
+get.prior.covar.with.all.max.step.no.pc <- function(X.c,max.step,lambda.mat, Q, factor.mat,omega.table,bma=TRUE)  {
+  test=list()
+  R=ncol(X.c)
+  M=nrow(X.c)
+  for(l in 1:nrow(omega.table)){
+    test[[l]]=list()
+    omega=omega.table[l,]
+    test[[l]][[1]]=omega*diag(1,R)
+    data.prox=max.step$true.covs[1,,]
+    d.norm=data.prox/max(diag(data.prox))
+    
+    
+    test[[l]][[2]]=omega*d.norm
+    
+    
+    
+
+    if(Q!=0){for(q in 1:Q){
+      
+      load=as.matrix(lambda.mat[,q])
+      fact=as.matrix(factor.mat[q,])
+      rank.prox=load%*%t(fact)
+      a=(1/M*(t(rank.prox)%*% rank.prox))
+      a[is.nan(a)] = 0
+      a.norm=a/max(diag(a))
+      test[[l]][[q+2]]=omega*a.norm
+    }}
+    full.rank=max.step$true.covs[2,,]
+    b.norm=full.rank/max(diag(full.rank))
+    test[[l]][[Q+3]]=omega*b.norm
+    
+    if(bma==TRUE){
+      configs=matrix(0,nrow=R,ncol=R)
+      
+      R=ncol(factor.mat)
+      for(r in 1:R){
+        configs[r,r]=1}
+      
+      configs=rbind(configs,rep(1,R))
+      for(c in 1:nrow(configs)) {
+        
+        mat=(configs[c,]%*%t(configs[c,]))
+        
+        test[[l]][[Q+3+c]]=omega*mat}}}
+  return(U.0kl=test)
+}
+
+
+
+compute.hm.covmat.all.max.step.no.pc = function(t.stat,v.j,Q,lambda.mat,A,factor.mat,max.step){
+  X.real=as.matrix(t.stat)
+  X.c=apply(X.real,2,function(x) x-mean(x)) ##Column centered matrix of t statistics
+  R=ncol(X.c)
+  omega=mult.tissue.grid(mult=sqrt(2),t.stat,v.j)
+  omega.table=data.frame(omega)
+  lambda.mat=lambda.mat
+  A=A
+  factor.mat=factor.mat
+  U.0kl=get.prior.covar.with.all.max.step.no.pc(X.c,max.step = max.step,lambda.mat = lambda.mat,Q = Q,factor.mat = factor.mat,omega.table=omega.table,bma = TRUE)
+  covmat=unlist(U.0kl,recursive=F)
+  saveRDS(covmat,paste0("covmat",A,".rds"))
+  
+  return(covmat)}
+
+
+
