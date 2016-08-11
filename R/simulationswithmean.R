a1056b402955dae2df68891d967cc33c02df4dd7
Sarah Margaret Urbut <sarahmurbut@gmail.com>
2016-08-11 18:35:32 -0500

    update

--- R/simulationswithmean.R
+++ R/simulationswithmean.R
@@ -1,18 +1,10 @@
----
-title: "simscriptformean"
-output: html_document
----
 
-This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
-
-When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
-
-```{r}
-#simulate from chat
+#' @title chat_sim
 #' @param n number of SNPs
-#' @param d number of tissues
-#' @param betasd the measurement error
-#' @param esd the variance of the residual matrix E in Y=LF+E
+#' @param d number of subgroups
+#' @param betasd the vriance of true effect
+#' @param esd the standard error of E in chat=mu+c+E, i.e., E~N(0,diag(esd^2))
+#' @export
 library()
 chat_sim=function(n=1000,d=3,betasd=1,esd=0.1,K=10){
   J=0.10*n
@@ -55,10 +47,3 @@ t=chat/sj
 
 ```
 
-You can also embed plots, for example:
-
-```{r, echo=FALSE}
-plot(cars)
-```
-
-Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
