b7f0b08ddffac74676bb92c90ba0d279dc23ba79
Sarah Margaret Urbut <sarahmurbut@gmail.com>
2016-10-21 13:17:57 -0500

    update

--- R/truthscripts.R
+++ R/truthscripts.R
@@ -59,5 +59,135 @@ if(zero==TRUE){covmat=c(test,list(matrix(rep(0,R*R),ncol=R,nrow=R)))}
   return(covmat)}
 
 
+#'@title compute.covmat.using.fixed.omega.withQ
+#'@param covmat a K list of covariance matrices
+#'@param b.gp.hat JxR matrix of mles
+#'@param se.gp.hat JxR matrix of standarderrors
+#'@export
 
+compute.covmat.using.fixed.omega.withQ=function(max.step,Q,bma=TRUE,omega=2,zero=TRUE,A,power=1)  {
+  test=list()
+  test[[1]]=omega*diag(1,R)
+    data.prox=max.step$true.covs[1,,]
+    d.norm=data.prox/max(diag(data.prox))
+    
+    
+    test[[2]]=omega*d.norm
+    
+    
+    
+    cov.pc=max.step$true.covs[3,,]
+    
+    
+    cov.pc.norm=cov.pc/max(diag(cov.pc))
+    
+    
+    
+    test[[3]]=omega*(cov.pc.norm)
+    if(Q!=0){for(q in 1:Q){
+      
+      
+      rank.prox=max.step$true.covs[3+q,,]
+      a=t(rank.prox)%*% rank.prox
+      a[is.nan(a)] = 0
+      a.norm=a/max(diag(a))
+      test[[q+3]]=omega*a.norm
+    }}
+    full.rank=max.step$true.covs[2,,]
+    b.norm=full.rank/max(diag(full.rank))
+    test[[Q+4]]=omega*b.norm
+    
+    if(bma==TRUE){
+      configs=matrix(0,nrow=R,ncol=R)
+      for(r in 1:R){
+        configs[r,r]=1}
+      
+      configs=rbind(configs,rep(1,R))
+      for(c in 1:nrow(configs)) {
+        
+        mat=(configs[c,]%*%t(configs[c,]))
+        
+        test[[Q+4+c]]=omega*mat}}
+  
+  covmat=test
+  
+  if(zero==TRUE){
+    z=matrix(rep(0,R*R),ncol=R,nrow=R)
+    covmat=c(covmat,list(z))
+  }
+  saveRDS(covmat,paste0("covmat",A,".rds"))
+  
+  return(list(covmat=covmat,omega))
 
+}
+
+#'@title compute.hm.mash.covmat.all.max.step.fixed.omega
+#'@param covmat a K list of covariance matrices
+#'@param b.gp.hat JxR matrix of mles
+#'@param se.gp.hat JxR matrix of standarderrors
+#'@export
+
+compute.hm.mash.covmat.all.max.step.fixed.omega=function(bma=TRUE,omega,Q,lambda.mat,A,factor.mat,max.step,zero=FALSE,maxp=1,minp=1,power=1){
+  
+  
+  
+  lambda.mat=lambda.mat
+  A=A
+  factor.mat=factor.mat
+  test=list()
+  R=dim(max.step$true.covs)[3]
+  test=list()
+    
+  test[[1]]=omega*diag(1,R)
+    data.prox=max.step$true.covs[1,,]
+    d.norm=data.prox/max(diag(data.prox))
+    
+    
+    test[[2]]=omega*d.norm
+    
+    
+    
+    cov.pc=max.step$true.covs[3,,]
+    
+    
+    cov.pc.norm=cov.pc/max(diag(cov.pc))
+    
+    
+    
+    test[[3]]=omega*(cov.pc.norm)
+    if(Q!=0){for(q in 1:Q){
+      
+      load=as.matrix(lambda.mat[,q])
+      fact=as.matrix(factor.mat[q,])
+      rank.prox=load%*%t(fact)
+      a=(t(rank.prox)%*% rank.prox)
+      a[is.nan(a)] = 0
+      a.norm=a/max(diag(a))
+      test[[q+3]]=omega*a.norm
+    }}
+    full.rank=max.step$true.covs[2,,]
+    b.norm=full.rank/max(diag(full.rank))
+    test[[Q+4]]=omega*b.norm
+    
+    if(bma==TRUE){
+      configs=matrix(0,nrow=R,ncol=R)
+      
+      R=ncol(factor.mat)
+      for(r in 1:R){
+        configs[r,r]=1}
+      
+      configs=rbind(configs,rep(1,R))
+      for(c in 1:nrow(configs)) {
+        
+        mat=(configs[c,]%*%t(configs[c,]))
+        
+        test[[Q+4+c]]=omega*mat}}
+  covmat=test
+  
+  if(zero==TRUE){
+    z=matrix(rep(0,R*R),ncol=R,nrow=R)
+    covmat=c(covmat,list(z))
+  }
+  saveRDS(covmat,paste0("covmat",A,".rds"))
+  
+  return(list(covmat=covmat,omega))}
