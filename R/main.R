772bc99c0373157e37d7662ec83049d45a094248
Sarah Margaret Urbut <sarahmurbut@gmail.com>
2017-03-27 09:53:57 -0500

    Updated grid func minp

--- R/main.R
+++ R/main.R
@@ -1676,7 +1676,7 @@ compute.covmat.with.heterogeneity.no.shrink = function(b.gp.hat,sebetahat,A,zero
 #' @return A list of covariance matrices
 #' @export
 
-compute.covmat.with.heterogeneity = function(b.gp.hat,sebetahat,A,zero=FALSE){
+compute.covmat.with.heterogeneity = function(b.gp.hat,sebetahat,A,zero=FALSE,maxp=1,minp=1){
   
   omega=mult.tissue.grid(mult=sqrt(2),b.gp.hat,sebetahat)
   
